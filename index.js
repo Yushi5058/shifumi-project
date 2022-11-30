7417fdc76329fb15a78317bf985b2d098b0ed8a3
chakroun-elias <eliaschakroun.01@gmail.com>
2022-11-30 12:22:30 +0100

    create index.js

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
-const choices = ["rock", "paper", "scissors"];
+const selectionButtons =  document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌️',
+        beats: 'paper'
+    }
+]
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const  selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+        })
+})
 
-function getComputerChoice (choices) {
-    return Math.random(Math * 3);
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+    declareWinner(yourScoreSpan, computerScoreSpan)
+
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
 }
 
-function playRound (playerSelection, computerSelection) {
-
-    // your code here
+function declareWinner(yourScoreSpan, computerScoreSpan) {
+    if (yourScoreSpan == 5) {
+        alert("You win!")
+    } else if (computerScoreSpan == 5) {
+        alert("You lose!")
+    }
 }
 
-const playerSelection = prompt(message);
-const computerSelection = getComputerChoice (choices);
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
 
-console.log(playRound(playerSelection, computerSelection));
+function addSelectionResult (selection, winner) {
+    const div = document.createElement('div')
+    div.innerText =  selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
 
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
 
-function game () {
-    for (let i= 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
-    };
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
